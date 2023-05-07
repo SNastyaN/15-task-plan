34b0bd9b5344917a99bfdf8e7a925ee8823b8eae
Anastasiia Sedalishcheva <ffttyyll@gmail.com>
2023-05-07 18:20:19 +0900

    new

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 let container = document.querySelector('.plan');
 let task = document.querySelector(".task");
-let buttonADD = document.querySelector('.add')
+let buttonADD = document.querySelector('.add');
+let buttonClear = document.querySelector('.clear');
 function addNewTask() {
+    const block = document.createElement('div');
+    block.className="containerfortasks"
     const newtask=document.createElement('div');
     newtask.className="new"
     newtask.innerText=task.value;
-    console.log(newtask) 
-    container.appendChild(newtask);
+    console.log(newtask)
+    let input = document.createElement('input'); // Создаём первый элемент
+        input.type = 'radio'; // Проставляем ему тип
+    block.appendChild(newtask);
+    block.appendChild(input);
     task.value='';
+    container.append(block);
+
+    buttonClear.addEventListener('click', ()=>{
+        container.textContent="НЕТ ЗАДАЧ";
+    })
+
+
+}
+
+
 
     //<ul id="parent"></ul>
 //let parent = document.querySelector('#parent');
@@ -16,7 +32,7 @@ function addNewTask() {
 	//let li = document.createElement('li');
 	//li.innerHTML = i;
 	//parent.append(li);
-}
+//}
     //let task = document.querySelector(".task").value;
     //container.textContent = task;
 //}
